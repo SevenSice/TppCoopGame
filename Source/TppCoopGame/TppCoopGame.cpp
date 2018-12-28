f1529406a23357c379ed86f0179a6d0c744e1d36
SevenSice <daiyipeng@outlook.com>
2018-12-28 15:51:27 +0800

    创建空工程。

---
+++ Source/TppCoopGame/TppCoopGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TppCoopGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TppCoopGame, "TppCoopGame" );
